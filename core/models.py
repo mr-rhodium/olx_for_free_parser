63d3b31a87fa22bf9b308e957cdd2e0beee7127e
Taras Nytiaha <taras.nytiaha@gmail.com>
2023-05-28 21:15:08 +0200

    fix: typo

--- core/models.py
+++ core/models.py
@@ -14,7 +14,7 @@ class StopWord(models.Model):
 
 
 
-class TelgramUser(Base):
+class TelegramUser(Base):
     id = models.BigIntegerField(primary_key=True, verbose_name="Telegram User ID")
     chat_id = models.BigIntegerField(verbose_name="Telegram Chat ID")
     username = models.CharField(max_length=64, null=True, verbose_name="Telegram Username")
