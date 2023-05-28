578e6d37bd2747ad3f0942f45b78bc8e0f36a324
Taras Nytiaha <taras.nytiaha@gmail.com>
2023-05-28 21:17:04 +0200

    feat: added TelegramUser model to admin

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import StopWord
+from .models import StopWord, TelegramUser
 
 
 class AdminWord(admin.ModelAdmin):
     pass
 
+class AdminTelegramUser(admin.ModelAdmin):
+    pass
 
 admin.site.register(StopWord, AdminWord)
+admin.site.register(TelegramUser, AdminTelegramUser)
