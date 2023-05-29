96469ef1a7bd4388e2a1e178ae2d808cc25128da
Taras Nytiaha <taras.nytiaha@gmail.com>
2023-05-29 10:30:38 +0200

    feat: added advert model

--- core/models.py
+++ core/models.py
@@ -10,24 +10,38 @@ class StopWord(models.Model):
 
     class Meta:
         verbose_name = "Stop word"
-        verbose_name_plural= "Stop words"
-
+        verbose_name_plural = "Stop words"
 
 
 class TelegramUser(Base):
     id = models.BigIntegerField(primary_key=True, verbose_name="Telegram User ID")
     chat_id = models.BigIntegerField(verbose_name="Telegram Chat ID")
-    username = models.CharField(max_length=64, null=True, verbose_name="Telegram Username")
-    active = models.BooleanField(default=False, verbose_name="Status is it available to send message")
-
+    username = models.CharField(
+        max_length=64, null=True, verbose_name="Telegram Username"
+    )
+    active = models.BooleanField(
+        default=False, verbose_name="Status is it available to send message"
+    )
 
     @property
     def available_to_send(self):
         return self.active
-    
+
     def __str__(self) -> str:
         return str(self.id)
-    
+
     class Meta:
         verbose_name = "Telegtam user"
         verbose_name_plural = "Telegtam users"
+
+
+class Advert(Base):
+    telegram_user = models.ForeignKey(TelegramUser, on_delete=models.CASCADE)
+    advert_id = models.CharField(max_length=10, verbose_name="Advert name")
+
+    def __str__(self) -> str:
+        return str(self.advert_id)
+
+    class Meta:
+        verbose_name = "Advert"
+        verbose_name_plural = "Adverts"
