9fe652725fee397f06720646f92e67f310527c54
Taras Nytiaha <taras.nytiaha@gmail.com>
2023-06-12 05:40:10 +0200

    feat: added settings for redis

--- bot/settings.py
+++ bot/settings.py
@@ -1,21 +1,37 @@
+from functools import lru_cache
+from pydantic import BaseSettings
+
+
 HEADERS: dict = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
 }
+# TODO need refactor
 URL: str = "https://www.olx.pl/api/v1/offers/?category_id=1151&city_id=17871&district_id=369&filter_enum_price=free&filter_refiners=spell_checker&limit=40&offset=0"
 BOT_THOKEN: str = "961019773:AAF271xwgF8lskKr8x7exxMgzfgNGOioUuY"
+TIME_OUT_REQUEST = 100
+TIME_OUT_REQUEST_LONG_POLLING = 60
 
 
-# from enum import Enum
+class RedisSettings(BaseSettings):
+    REDIS_HOST: str = ""
+    REDIS_PORT: int = 0
+    REDIS_DB: int = 0
+    REDIS_PASSWORD: str = ""
+    TTl = 60 * 60 * 24  # 1 day
 
-# from app.config import env
+    @property
+    def ttl(self) -> int:
+        return int(self.TTl)
+
+    @property
+    def url(self) -> str:
+        return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}/{self.REDIS_DB}"
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"  # noqa: A003
 
 
-# class RunningMode(str, Enum):
-#     LONG_POLLING = "LONG_POLLING"
-#     WEBHOOK = "WEBHOOK"
-
-
-# TG_TOKEN = env("TG_TOKEN", cast=str)
-
-# RUNNING_MODE = env("RUNNING_MODE", cast=RunningMode, default=RunningMode.LONG_POLLING)
-# WEBHOOK_URL = env("WEBHOOK_URL", cast=str, default="")
+@lru_cache()
+def redis_settings() -> RedisSettings:
+    return RedisSettings()
